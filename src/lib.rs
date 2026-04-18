5ae55c558d32dd38fd80f5c3d80297aff106a4d6
Catsack <tnnic82@gmail.com>
2026-04-18 19:59:20 +0300

    Add test subscribe

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+use actix_web::{App, HttpResponse, HttpServer, Responder, dev::Server, web};
+use std::net::TcpListener;
+
+async fn health_check() -> HttpResponse{
+    HttpResponse::Ok().finish()
+}
+
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error>{
+    let server = HttpServer::new(||{
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+    })
+    .listen(listener)?
+    .run();
+    Ok(server)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,7 @@
-use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, Route, guard, web};
+use zero2prod::run;
 
-// async fn greet(req: HttpRequest) -> impl Responder{
-//     let name = req.match_info().get("name").unwrap_or("World");
-//     format!("Hello {}!", &name)
-// }
-async fn health_check(_req: HttpRequest) -> impl Responder{
-    HttpResponse::Ok()
-}
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error>{
-    HttpServer::new(||{
-        App::new()
-            // .route("/", web::get().to(greet))
-            // .route("/{name}", web::get().to(greet))
-            .route("/health_check", web::get().to(health_check))
-    })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
+    let server = run("127.0.0.1:0".into())?.await;
+    Ok(())
 }
